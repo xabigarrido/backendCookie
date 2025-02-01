c6109d49a24008bc33d2ecc10f66f892eae0243c
xabigarrido <xabigarrido@gmail.com>
2025-02-02 00:25:44 +0100

    euu

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://backendcookie.onrender.com", credentials: true })
+);
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.get("/", (req, res) => {
